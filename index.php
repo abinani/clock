aa193e92a0bc54e19c9191e1f0c531b668dad1b5
abinani <abinani@fas.harvard.edu>
2014-07-02 17:27:48 -0400

    First cut - Clock

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>What is the time?</title>
+    <link rel='stylesheet' href='css/style.css' type='text/css'>
+    <?php require 'logic.php' ?>
+</head>
+<body class="<?echo $class_value;?>">
+    <img src="http://making-the-internet.s3.amazonaws.com/<?echo $img;?>" alt="<?echo $img;?>">
+</body>
+</html>
---
+++ logic.php
@@ -0,0 +1,28 @@
+<?php 
+$morning = mktime(5);
+$afternoon = mktime(11);
+$evening = mktime(12+4);
+$night   = mktime(12+8);
+
+$current = mktime(date("G"));
+$img = "";
+$class_value = "INIT";
+
+if($current >= $morning && $current < $afternoon) {
+    $img = "php-morning.png";
+    $class_value = "morning";
+}
+else if($current >= $afternoon && $current < $evening)
+{
+    $img = "php-afternoon.png";
+    $class_value = "afternoon";
+}
+else if($current >= $evening && $current < $night)
+{
+   $img = "php-evening.png";
+   $class_value = "evening";
+}
+else {
+    $img = "php-night.png";
+    $class_value = "night";
+}
